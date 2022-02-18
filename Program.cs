ce65b47498c88f6036274a7accd8e142ae25eb08
Deepali Sagar <Deepali Sagar@DESKTOP-JLEC72J>
2022-02-18 14:21:47 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Myapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Celsius value :");
+            double cel = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Kelvin={0}", cel + 273);
+            Console.WriteLine("Fahrenheit={0}", cel * 18 / 10 + 32);
+        }
+    }
+}
+
